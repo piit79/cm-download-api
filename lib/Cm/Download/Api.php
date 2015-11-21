9f8ba53c43fb52c0c18ae7aac820c8e3537687b7
Petr Sedlacek <petr@sedlacek.biz>
2015-11-21 23:59:58 +0100

    Switch to braced namespaces

--- lib/Cm/Download/Api.php
+++ lib/Cm/Download/Api.php
@@ -1,300 +1,301 @@
 <?php
 
-namespace Cm\Download;
+namespace Cm\Download {
 
 
-class Api
-{
-
-    const URI_API = '/api';
-    const URI_API_V1 = '/api/v1/';
-
-    const TYPE_TEXT = "text/plain";
-    const TYPE_JSON = "application/json";
-
-    /**
-     * Build filesystem root
-     *
-     * @var string
-     */
-    private $root;
-
-    /**
-     * Build filesystem base URL
-     *
-     * @var string
-     */
-    private $base_url;
-
-    /**
-     * Request content type
-     *
-     * @var string
-     */
-    private $content_type;
-
-    /**
-     * Request method (GET/POST...)
-     *
-     * @var string
-     */
-    private $request_method;
-
-    /**
-     * Request URI
-     *
-     * @var string
-     */
-    private $uri;
-
-    /**
-     * Request URI parts split by /
-     *
-     * @var array
-     */
-    private $uri_parts;
-
-    /**
-     * Array containing request decoded from json
-     *
-     * @var array
-     */
-    private $request;
-
-    /**
-     * CmDownloadApi constructor.
-     *
-     * @param string $root
-     * @param string $base_url
-     */
-    function __construct($root, $base_url)
+    class Api
     {
-        $this->root = $root;
-        $this->base_url = $base_url;
-    }
 
-    /**
-     *
-     */
-    protected function getRequest()
-    {
-        $this->request_method = $_SERVER['REQUEST_METHOD'];
-        $this->content_type = $this->getContentType();
-        $this->uri = $_SERVER['REQUEST_URI'];
-        $uri_parts = explode('/', $this->uri);
-        $this->uri_parts = array();
-        foreach ($uri_parts as $part) {
-            if ($part != "") {
-                $this->uri_parts[] = $part;
-            }
+        const URI_API = '/api';
+        const URI_API_V1 = '/api/v1/';
+
+        const TYPE_TEXT = "text/plain";
+        const TYPE_JSON = "application/json";
+
+        /**
+         * Build filesystem root
+         *
+         * @var string
+         */
+        private $root;
+
+        /**
+         * Build filesystem base URL
+         *
+         * @var string
+         */
+        private $base_url;
+
+        /**
+         * Request content type
+         *
+         * @var string
+         */
+        private $content_type;
+
+        /**
+         * Request method (GET/POST...)
+         *
+         * @var string
+         */
+        private $request_method;
+
+        /**
+         * Request URI
+         *
+         * @var string
+         */
+        private $uri;
+
+        /**
+         * Request URI parts split by /
+         *
+         * @var array
+         */
+        private $uri_parts;
+
+        /**
+         * Array containing request decoded from json
+         *
+         * @var array
+         */
+        private $request;
+
+        /**
+         * CmDownloadApi constructor.
+         *
+         * @param string $root
+         * @param string $base_url
+         */
+        function __construct($root, $base_url)
+        {
+            $this->root = $root;
+            $this->base_url = $base_url;
         }
 
-        switch ($this->request_method) {
-            case 'POST':
-                $this->request = $this->decodeRequest();
-                break;
-
-            default:
-                $this->response(405, self::TYPE_TEXT, "405 Method Not Allowed\n\nThe method GET is not allowed for this resource.");
-                exit();
-        }
-    }
-
-    /**
-     * Get request content type
-     *
-     * @return string
-     */
-    protected function getContentType()
-    {
-        $content_type_value = $_SERVER['CONTENT_TYPE'];
-        if (strpos($content_type_value, ';') === FALSE) {
-            $content_type = $content_type_value;
-        } else {
-            list($content_type) = explode(';', $content_type_value);
-        }
-
-        return $content_type;
-    }
-
-    /**
-     * Decode the input data
-     *
-     * @return array
-     */
-    protected function decodeRequest()
-    {
-        $request_data = file_get_contents("php://input");
-
-        switch ($this->content_type) {
-            case self::TYPE_JSON:
-                $json = json_decode($request_data, TRUE);
-                return $json;
-                break;
-        }
-
-        return FALSE;
-    }
-
-    /**
-     * Get API call name
-     *
-     * @return bool
-     */
-    protected function getApiCall()
-    {
-        if ($this->uri == self::URI_API) {
-            if (isset($this->request['method'])
-                && $this->request['method']
-                && method_exists($this, $this->request['method'])
-            ) {
-                return $this->request['method'];
-            }
-        } elseif (strpos($this->uri, self::URI_API_V1) === 0) {
-            reset($this->uri_parts);
-            $method = end($this->uri_parts);
-            if (method_exists($this, $method)) {
-                return $method;
-            }
-        }
-        return FALSE;
-    }
-
-    /**
-     * Handle the API request
-     *
-     * @return bool
-     */
-    public function handle()
-    {
-        $this->getRequest();
-        $apiCall = $this->getApiCall();
-        if (!$apiCall) {
-            $output = array(
-                'id' => NULL,
-                'result' => NULL,
-                'error' => "Error decoding JSON",
-            );
-            $this->response(200, self::TYPE_JSON, $output);
-            exit();
-        }
-        return $this->$apiCall();
-    }
-
-    /**
-     * Output a HTTP code along with result data
-     *
-     * @param $code int
-     * @param $content_type string
-     * @param string $message string|array
-     */
-    protected function response($code, $content_type, $message = "")
-    {
-        http_response_code($code);
-        header('Content-Type: ' . $content_type);
-        switch ($content_type) {
-            case self::TYPE_TEXT:
-                echo $message;
-                break;
-
-            case self::TYPE_JSON:
-                echo json_encode($message, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
-                break;
-
-            default:
-                echo $message;
-        }
-    }
-
-    /**
-     * Return the date part of the build filename
-     *
-     * @param string $filename
-     * @return string The date of the build in YYYYMMDD format
-     */
-    protected function getBuildDate($filename) {
-        $parts = explode('-', $filename);
-        return $parts[2];
-    }
-
-    /**
-     * get_all_builds API call
-     */
-    protected function get_all_builds()
-    {
-        if (!isset($this->request['params']) || !is_array($this->request['params'])
-            || !isset($this->request['params']['device']) || !$this->request['params']['device']) {
-            // invalid request
-        }
-        $device = $this->request['params']['device'];
-        $channel = "nightly";
-        $api_level = 23;
-        $device_dir = $this->root . DIRECTORY_SEPARATOR . $device;
-        if (is_dir($device_dir)) {
-            $files = scandir($device_dir);
-        } else {
-            $files = array();
-        }
-        $output = array();
-        $output['id'] = NULL;
-        $output['error'] = NULL;
-        $result = array();
-        foreach ($files as $filename) {
-            $file_path_rel = $device . DIRECTORY_SEPARATOR . $filename;
-            $file_path_full = $this->root . DIRECTORY_SEPARATOR . $file_path_rel;
-            if (!is_file($file_path_full) || substr($filename, -4) != ".zip") {
-                continue;
-            }
-            $file_url = $this->base_url . '/' . $file_path_rel;
-            $stat = stat($file_path_full);
-            $timestamp = $stat[9];
-            $md5sum_file = $file_path_full . ".md5sum";
-            $md5sum = NULL;
-            if (is_file($md5sum_file)) {
-                $md5sum_contents = file_get_contents($md5sum_file);
-                list($md5sum_line) = explode("\n", $md5sum_contents);
-                if (preg_match('/^([0-9a-f]{32}) /', $md5sum_line, $m)) {
-                    $md5sum = $m[1];
+        /**
+         *
+         */
+        protected function getRequest()
+        {
+            $this->request_method = $_SERVER['REQUEST_METHOD'];
+            $this->content_type = $this->getContentType();
+            $this->uri = $_SERVER['REQUEST_URI'];
+            $uri_parts = explode('/', $this->uri);
+            $this->uri_parts = array();
+            foreach ($uri_parts as $part) {
+                if ($part != "") {
+                    $this->uri_parts[] = $part;
                 }
             }
-            $incremental = "4a97bfd9e2";
-            $changes = $this->base_url . '/' . $device . '/' . str_replace(".zip", ".changes", $filename);
-            $build = Api\Build::factory()
-                ->setUrl($file_url)
-                ->setFilename($filename)
-                ->setTimestamp($timestamp)
-                ->setMd5sum($md5sum)
-                ->setIncremental($incremental)
-                ->setChanges($changes)
-                ->setChannel($channel)
-                ->setApiLevel($api_level);
-            $result[] = $build->toArray();
-        }
-        $output['result'] = $result;
-        $this->response(200, self::TYPE_JSON, $output);
-    }
 
-    /**
-     * get_delta API call - stub, always returns 500
-     */
-    protected function get_delta()
-    {
-        $response_invalid_input = array(
-            'errors' => array(
-                array(
-                    'reason' => "internal server error",
-                    'message' => "Internal Server Error",
+            switch ($this->request_method) {
+                case 'POST':
+                    $this->request = $this->decodeRequest();
+                    break;
+
+                default:
+                    $this->response(405, self::TYPE_TEXT, "405 Method Not Allowed\n\nThe method GET is not allowed for this resource.");
+                    exit();
+            }
+        }
+
+        /**
+         * Get request content type
+         *
+         * @return string
+         */
+        protected function getContentType()
+        {
+            $content_type_value = $_SERVER['CONTENT_TYPE'];
+            if (strpos($content_type_value, ';') === FALSE) {
+                $content_type = $content_type_value;
+            } else {
+                list($content_type) = explode(';', $content_type_value);
+            }
+
+            return $content_type;
+        }
+
+        /**
+         * Decode the input data
+         *
+         * @return array
+         */
+        protected function decodeRequest()
+        {
+            $request_data = file_get_contents("php://input");
+
+            switch ($this->content_type) {
+                case self::TYPE_JSON:
+                    $json = json_decode($request_data, TRUE);
+                    return $json;
+                    break;
+            }
+
+            return FALSE;
+        }
+
+        /**
+         * Get API call name
+         *
+         * @return bool
+         */
+        protected function getApiCall()
+        {
+            if ($this->uri == self::URI_API) {
+                if (isset($this->request['method'])
+                    && $this->request['method']
+                    && method_exists($this, $this->request['method'])
+                ) {
+                    return $this->request['method'];
+                }
+            } elseif (strpos($this->uri, self::URI_API_V1) === 0) {
+                reset($this->uri_parts);
+                $method = end($this->uri_parts);
+                if (method_exists($this, $method)) {
+                    return $method;
+                }
+            }
+            return FALSE;
+        }
+
+        /**
+         * Handle the API request
+         *
+         * @return bool
+         */
+        public function handle()
+        {
+            $this->getRequest();
+            $apiCall = $this->getApiCall();
+            if (!$apiCall) {
+                $output = array(
+                    'id' => NULL,
+                    'result' => NULL,
+                    'error' => "Error decoding JSON",
+                );
+                $this->response(200, self::TYPE_JSON, $output);
+                exit();
+            }
+            return $this->$apiCall();
+        }
+
+        /**
+         * Output a HTTP code along with result data
+         *
+         * @param $code int
+         * @param $content_type string
+         * @param string $message string|array
+         */
+        protected function response($code, $content_type, $message = "")
+        {
+            http_response_code($code);
+            header('Content-Type: ' . $content_type);
+            switch ($content_type) {
+                case self::TYPE_TEXT:
+                    echo $message;
+                    break;
+
+                case self::TYPE_JSON:
+                    echo json_encode($message, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+                    break;
+
+                default:
+                    echo $message;
+            }
+        }
+
+        /**
+         * Return the date part of the build filename
+         *
+         * @param string $filename
+         * @return string The date of the build in YYYYMMDD format
+         */
+        protected function getBuildDate($filename) {
+            $parts = explode('-', $filename);
+            return $parts[2];
+        }
+
+        /**
+         * get_all_builds API call
+         */
+        protected function get_all_builds()
+        {
+            if (!isset($this->request['params']) || !is_array($this->request['params'])
+                || !isset($this->request['params']['device']) || !$this->request['params']['device']) {
+                // invalid request
+            }
+            $device = $this->request['params']['device'];
+            $channel = "nightly";
+            $api_level = 23;
+            $device_dir = $this->root . DIRECTORY_SEPARATOR . $device;
+            if (is_dir($device_dir)) {
+                $files = scandir($device_dir);
+            } else {
+                $files = array();
+            }
+            $output = array();
+            $output['id'] = NULL;
+            $output['error'] = NULL;
+            $result = array();
+            foreach ($files as $filename) {
+                $file_path_rel = $device . DIRECTORY_SEPARATOR . $filename;
+                $file_path_full = $this->root . DIRECTORY_SEPARATOR . $file_path_rel;
+                if (!is_file($file_path_full) || substr($filename, -4) != ".zip") {
+                    continue;
+                }
+                $file_url = $this->base_url . '/' . $file_path_rel;
+                $stat = stat($file_path_full);
+                $timestamp = $stat[9];
+                $md5sum_file = $file_path_full . ".md5sum";
+                $md5sum = NULL;
+                if (is_file($md5sum_file)) {
+                    $md5sum_contents = file_get_contents($md5sum_file);
+                    list($md5sum_line) = explode("\n", $md5sum_contents);
+                    if (preg_match('/^([0-9a-f]{32}) /', $md5sum_line, $m)) {
+                        $md5sum = $m[1];
+                    }
+                }
+                $incremental = "4a97bfd9e2";
+                $changes = $this->base_url . '/' . $device . '/' . str_replace(".zip", ".changes", $filename);
+                $build = Api\Build::factory()
+                    ->setUrl($file_url)
+                    ->setFilename($filename)
+                    ->setTimestamp($timestamp)
+                    ->setMd5sum($md5sum)
+                    ->setIncremental($incremental)
+                    ->setChanges($changes)
+                    ->setChannel($channel)
+                    ->setApiLevel($api_level);
+                $result[] = $build->toArray();
+            }
+            $output['result'] = $result;
+            $this->response(200, self::TYPE_JSON, $output);
+        }
+
+        /**
+         * get_delta API call - stub, always returns 500
+         */
+        protected function get_delta()
+        {
+            $response_invalid_input = array(
+                'errors' => array(
+                    array(
+                        'reason' => "internal server error",
+                        'message' => "Internal Server Error",
+                    ),
                 ),
-            ),
-        );
-        $response_delta_not_found = array(
-            'errors' => array(
-                array(
-                    'message' => "Unable to find delta",
+            );
+            $response_delta_not_found = array(
+                'errors' => array(
+                    array(
+                        'message' => "Unable to find delta",
+                    ),
                 ),
-            ),
-        );
-        $this->response(200, self::TYPE_JSON, $response_delta_not_found);
+            );
+            $this->response(200, self::TYPE_JSON, $response_delta_not_found);
+        }
     }
 }
 
--- lib/Cm/Download/Api/Build.php
+++ lib/Cm/Download/Api/Build.php
@@ -1,240 +1,241 @@
 <?php
 
-namespace Cm\Download\Api;
+namespace Cm\Download\Api {
 
 
-class Build
-{
-    /**
-     * @var string
-     */
-    private $url;
-
-    /**
-     * @var string
-     */
-    private $filename;
-
-    /**
-     * @var int
-     */
-    private $timestamp;
-
-    /**
-     * @var string
-     */
-    private $md5sum;
-
-    /**
-     * @var string
-     */
-    private $incremental;
-
-    /**
-     * @var string
-     */
-    private $changes;
-
-    /**
-     * @var string
-     */
-    private $channel;
-
-    /**
-     * @var string
-     */
-    private $apiLevel;
-
-    /**
-     * Build constructor.
-     * @param string $filename
-     * @param int $timestamp
-     * @param string $md5sum
-     * @param string $incremental
-     * @param string $changes
-     * @param string $channel
-     * @param string $api_level
-     */
-    public function __construct($filename = "", $timestamp = NULL, $md5sum = "", $incremental = "", $changes = "", $channel = "", $api_level = NULL)
+    class Build
     {
-        $this->filename = $filename;
-        $this->timestamp = $timestamp;
-        $this->md5sum = $md5sum;
-        $this->incremental = $incremental;
-        $this->changes = $changes;
-        $this->channel = $channel;
-        $this->apiLevel = $api_level;
-    }
+        /**
+         * @var string
+         */
+        private $url;
 
-    /**
-     * Factory method
-     * @return Build
-     */
-    public static function factory()
-    {
-        return new self();
-    }
+        /**
+         * @var string
+         */
+        private $filename;
 
-    /**
-     * @return string
-     */
-    public function getUrl()
-    {
-        return $this->url;
-    }
+        /**
+         * @var int
+         */
+        private $timestamp;
 
-    /**
-     * @param string $url
-     * @return Build updated object
-     */
-    public function setUrl($url)
-    {
-        $this->url = $url;
-        return $this;
-    }
+        /**
+         * @var string
+         */
+        private $md5sum;
 
-    /**
-     * @return string
-     */
-    public function getFilename()
-    {
-        return $this->filename;
-    }
+        /**
+         * @var string
+         */
+        private $incremental;
 
-    /**
-     * @param string $filename
-     * @return Build updated object
-     */
-    public function setFilename($filename)
-    {
-        $this->filename = $filename;
-        return $this;
-    }
+        /**
+         * @var string
+         */
+        private $changes;
 
-    /**
-     * @return int
-     */
-    public function getTimestamp()
-    {
-        return $this->timestamp;
-    }
+        /**
+         * @var string
+         */
+        private $channel;
 
-    /**
-     * @param int $timestamp
-     * @return Build updated object
-     */
-    public function setTimestamp($timestamp)
-    {
-        $this->timestamp = $timestamp;
-        return $this;
-    }
+        /**
+         * @var string
+         */
+        private $apiLevel;
 
-    /**
-     * @return string
-     */
-    public function getMd5sum()
-    {
-        return $this->md5sum;
-    }
+        /**
+         * Build constructor.
+         * @param string $filename
+         * @param int $timestamp
+         * @param string $md5sum
+         * @param string $incremental
+         * @param string $changes
+         * @param string $channel
+         * @param string $api_level
+         */
+        public function __construct($filename = "", $timestamp = NULL, $md5sum = "", $incremental = "", $changes = "", $channel = "", $api_level = NULL)
+        {
+            $this->filename = $filename;
+            $this->timestamp = $timestamp;
+            $this->md5sum = $md5sum;
+            $this->incremental = $incremental;
+            $this->changes = $changes;
+            $this->channel = $channel;
+            $this->apiLevel = $api_level;
+        }
 
-    /**
-     * @param string $md5sum
-     * @return Build updated object
-     */
-    public function setMd5sum($md5sum)
-    {
-        $this->md5sum = $md5sum;
-        return $this;
-    }
+        /**
+         * Factory method
+         * @return Build
+         */
+        public static function factory()
+        {
+            return new self();
+        }
 
-    /**
-     * @return string
-     */
-    public function getIncremental()
-    {
-        return $this->incremental;
-    }
+        /**
+         * @return string
+         */
+        public function getUrl()
+        {
+            return $this->url;
+        }
 
-    /**
-     * @param string $incremental
-     * @return Build updated object
-     */
-    public function setIncremental($incremental)
-    {
-        $this->incremental = $incremental;
-        return $this;
-    }
+        /**
+         * @param string $url
+         * @return Build updated object
+         */
+        public function setUrl($url)
+        {
+            $this->url = $url;
+            return $this;
+        }
 
-    /**
-     * @return string
-     */
-    public function getChanges()
-    {
-        return $this->changes;
-    }
+        /**
+         * @return string
+         */
+        public function getFilename()
+        {
+            return $this->filename;
+        }
 
-    /**
-     * @param string $changes
-     * @return Build updated object
-     */
-    public function setChanges($changes)
-    {
-        $this->changes = $changes;
-        return $this;
-    }
+        /**
+         * @param string $filename
+         * @return Build updated object
+         */
+        public function setFilename($filename)
+        {
+            $this->filename = $filename;
+            return $this;
+        }
 
-    /**
-     * @return string
-     */
-    public function getChannel()
-    {
-        return $this->channel;
-    }
+        /**
+         * @return int
+         */
+        public function getTimestamp()
+        {
+            return $this->timestamp;
+        }
 
-    /**
-     * @param string $channel
-     * @return Build updated object
-     */
-    public function setChannel($channel)
-    {
-        $this->channel = $channel;
-        return $this;
-    }
+        /**
+         * @param int $timestamp
+         * @return Build updated object
+         */
+        public function setTimestamp($timestamp)
+        {
+            $this->timestamp = $timestamp;
+            return $this;
+        }
 
-    /**
-     * @return string
-     */
-    public function getApiLevel()
-    {
-        return $this->apiLevel;
-    }
+        /**
+         * @return string
+         */
+        public function getMd5sum()
+        {
+            return $this->md5sum;
+        }
 
-    /**
-     * @param string $apiLevel
-     * @return Build updated object
-     */
-    public function setApiLevel($apiLevel)
-    {
-        $this->apiLevel = $apiLevel;
-        return $this;
-    }
+        /**
+         * @param string $md5sum
+         * @return Build updated object
+         */
+        public function setMd5sum($md5sum)
+        {
+            $this->md5sum = $md5sum;
+            return $this;
+        }
 
-    /**
-     * Return the build info as array
-     * @return array
-     */
-    public function toArray()
-    {
-        return array(
-            'url' => $this->url,
-            'filename' => $this->filename,
-            'timestamp' => $this->timestamp,
-            'md5sum' => $this->md5sum,
-            'incremental' => $this->incremental,
-            'changes' => $this->changes,
-            'channel' => $this->channel,
-            'api_level' => $this->apiLevel,
-        );
-    }
+        /**
+         * @return string
+         */
+        public function getIncremental()
+        {
+            return $this->incremental;
+        }
 
+        /**
+         * @param string $incremental
+         * @return Build updated object
+         */
+        public function setIncremental($incremental)
+        {
+            $this->incremental = $incremental;
+            return $this;
+        }
+
+        /**
+         * @return string
+         */
+        public function getChanges()
+        {
+            return $this->changes;
+        }
+
+        /**
+         * @param string $changes
+         * @return Build updated object
+         */
+        public function setChanges($changes)
+        {
+            $this->changes = $changes;
+            return $this;
+        }
+
+        /**
+         * @return string
+         */
+        public function getChannel()
+        {
+            return $this->channel;
+        }
+
+        /**
+         * @param string $channel
+         * @return Build updated object
+         */
+        public function setChannel($channel)
+        {
+            $this->channel = $channel;
+            return $this;
+        }
+
+        /**
+         * @return string
+         */
+        public function getApiLevel()
+        {
+            return $this->apiLevel;
+        }
+
+        /**
+         * @param string $apiLevel
+         * @return Build updated object
+         */
+        public function setApiLevel($apiLevel)
+        {
+            $this->apiLevel = $apiLevel;
+            return $this;
+        }
+
+        /**
+         * Return the build info as array
+         * @return array
+         */
+        public function toArray()
+        {
+            return array(
+                'url' => $this->url,
+                'filename' => $this->filename,
+                'timestamp' => $this->timestamp,
+                'md5sum' => $this->md5sum,
+                'incremental' => $this->incremental,
+                'changes' => $this->changes,
+                'channel' => $this->channel,
+                'api_level' => $this->apiLevel,
+            );
+        }
+
+    }
 }
