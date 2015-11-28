6994503d375292020827804668cba891ed692486
Petr Sedlacek <petr@sedlacek.biz>
2015-11-29 00:17:17 +0100

    Handle the HTTP request using a \Fw\Http\Request class

--- lib/Fw/Http.php
+++ lib/Fw/Http.php
@@ -5,6 +5,17 @@ namespace Fw {
 
     class Http
     {
+        const SCHEME_HTTP = "http";
+        const SCHEME_HTTPS = "https";
+
+        const METHOD_GET = "GET";
+        const METHOD_HEAD = "HEAD";
+        const METHOD_POST = "POST";
+        const METHOD_PUT = "PUT";
+        const METHOD_PATCH = "PATCH";
+        const METHOD_DELETE = "DELETE";
+        const METHOD_OPTIONS = "OPTIONS";
+
         const CONTENT_TYPE_TEXT = "text/plain";
         const CONTENT_TYPE_JSON = "application/json";
     }
