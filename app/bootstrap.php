8b84e6322eda2388340dc20e262e332bfe5e0dec
Petr Sedlacek <petr@sedlacek.biz>
2015-11-29 01:23:27 +0100

    Move bootstrap and config into new directory app

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,10 +1,11 @@
 <?php
 
-require_once 'lib/Fw/Loader.php';
-
-define('APP_DIR', realpath(__DIR__));
+define('APP_DIR', realpath(__DIR__ . DIRECTORY_SEPARATOR . ".."));
 define('LIB_DIR', APP_DIR . DIRECTORY_SEPARATOR . "lib");
 define('DEBUG', false);
+
+require_once 'config.php';
+require_once LIB_DIR . '/Fw/Loader.php';
 
 $loader = new \Fw\Loader(LIB_DIR);
 $loader->registerNamespaces(
