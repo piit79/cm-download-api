4810697df7dd4b6e90d6c5586b5e12a0ac42d269
Petr Sedlacek <petr@sedlacek.biz>
2015-12-29 15:51:49 +0100

    Add sample config file

---
+++ app/config.sample.php
@@ -0,0 +1,6 @@
+<?php
+
+// edit these constants to match your web server configuration
+// and rename the file to config.php
+define("DOWNLOAD_ROOT", "/var/www/cm");
+define("DOWNLOAD_BASE_URL", "http://cm.example.net/get");
